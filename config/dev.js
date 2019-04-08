4312e948d453cdd87ca482b2bec64efde28fbd49
Gupta <Varun.Gupta3@Honeywell.com>
2019-04-08 16:00:08 +0530

    Set up separate environments

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '800693876456-4suik4b0ftf13d7rncfkvpagskvcppjt.apps.googleusercontent.com',
+    googleClientSecret: '1_6AAe9WodHHgLIQx0PMvOLP',
+    mongoURI: 'mongodb://allenghost:varunak47@ds231956.mlab.com:31956/emaily-dev1',
+    cookieKey: 'hddadhb13y7812ch478c31m2hc3g6176m4121c231m47gh78hhc3'
+}
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '800693876456-4suik4b0ftf13d7rncfkvpagskvcppjt.apps.googleusercontent.com',
-    googleClientSecret: '1_6AAe9WodHHgLIQx0PMvOLP',
-    mongoURI: 'mongodb://allenghost:varunak47@ds231956.mlab.com:31956/emaily-dev1',
-    cookieKey: 'hddadhb13y7812ch478c31m2hc3g6176m4121c231m47gh78hhc3'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+}else{
+    module.exports = require('./dev');
 }
