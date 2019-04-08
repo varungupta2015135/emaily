07787297939aa3528dbec1c6936021c20cc37b57
Varun Gupta <varungupta2015135@gmail.com>
2019-04-08 08:11:47 +0530

    set up authentication

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const keys = require('./config/keys');
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
-    .then(() => {
-        console.log("Connected");
-    })
-    .catch(err => {
-        console.log(err);
-    })
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 2000;
--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,16 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -11,11 +21,19 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    (accessToken, refreshToken, profile) => {
-      console.log('profile: ', profile);
-      new User({
-        googleId: profile.id
-      }).save();
+    (accessToken, refreshToken, profile, done) => {
+      console.log("profile: ", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (!existingUser) {
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        } else {
+          done(null, existingUser);
+        }
+      });
     }
   )
 );
